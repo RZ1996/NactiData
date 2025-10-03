e957310d5e0c8590263304aa232f5d97f3eff2b9
Róbert Zámočník <zamocnikrobert@gmail.com>
2025-10-03 17:49:56 +0200

    Add project files.

---
+++ NactiData/Data.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+
+namespace NactiData
+{
+    public class Data
+    {
+        private double firstNumber;
+        private double secondNumber;
+        private double thirdNumber;
+
+        public Data() { }
+
+        public Data(double firstNumber, double secondNumber, double thirdNumber)
+        {
+            this.firstNumber = firstNumber;
+            this.secondNumber = secondNumber;
+            this.thirdNumber = thirdNumber;
+
+        }
+
+        public void loadData()
+        {
+            bool value = true;
+            while (value == true)
+            {
+                Console.WriteLine("Zadejte čísla: ");
+                try
+                {
+                    Console.Write("První zadané číslo: ");
+                    firstNumber = Double.Parse(Console.ReadLine());
+                    Console.Write("Druhé zadané číslo: ");
+                    secondNumber = Double.Parse(Console.ReadLine());
+                    Console.Write("Třetí zadané číslo: ");
+                    thirdNumber = Double.Parse(Console.ReadLine());
+                    value = false;
+                }
+                catch
+                {
+                    Console.WriteLine("Zadejte číselnou hodnotu!");
+                    value = true;
+                }
+            }
+        }
+
+        public void checkOperations()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Zadejte znak pro sčítání (+): ");
+                    char sumChar = Console.ReadKey(intercept: true).KeyChar;
+                    Console.WriteLine(sumChar);
+                    if (sumChar != '+') throw new ArgumentException("Očekáván znak '+'.");
+
+                    Console.Write("Zadejte znak pro násobení (*): ");
+                    char multipleChar = Console.ReadKey(intercept: true).KeyChar;
+                    Console.WriteLine(multipleChar);
+                    if (multipleChar != '*') throw new ArgumentException("Očekáván znak '*'.");
+
+                    Console.Write("Zadejte znak pro dělení (/): ");
+                    char sumAndDivChar = Console.ReadKey(intercept: true).KeyChar;
+                    Console.WriteLine(sumAndDivChar);
+                    if (sumAndDivChar != '/') throw new ArgumentException("Očekáván znak '/'.");
+
+                    // všechno je správně → ukončit smyčku
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Neplatný vstup: {ex.Message}");
+                    Console.WriteLine("Zkuste to znovu.\n");
+                }
+            }
+        }
+
+
+        public double sum()
+        {
+            return firstNumber + secondNumber + thirdNumber;
+        }
+
+        public double multiple()
+        {
+            return firstNumber * secondNumber * thirdNumber;
+        }
+
+        public double sumAndDiv()
+        {
+            if (thirdNumber == 0)
+                throw new DivideByZeroException("Nelze dělit nulou (třetí číslo je 0).");
+
+            return (firstNumber + secondNumber) / thirdNumber;
+        }
+
+    }
+    }
---
+++ NactiData/Program.cs
@@ -0,0 +1,9 @@
+﻿using NactiData;
+
+Data data = new Data();
+data.loadData();
+data.checkOperations();
+Console.WriteLine();
+Console.WriteLine(data.sum());
+Console.WriteLine(data.multiple());
+Console.WriteLine(data.sumAndDiv());
